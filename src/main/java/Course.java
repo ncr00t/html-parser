4ba903c8ada42b8216628fa77603793fccc188e2
Ivanisenko Roman <r.ivanisenko@gmail.com>
2017-08-15 17:31:50 +0300

    added class decribing course

---
+++ src/main/java/Course.java
@@ -0,0 +1,32 @@
+/**
+ * Class containing information about the course
+ */
+public class Course {
+    private String title;
+    private String url;
+    private int price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
