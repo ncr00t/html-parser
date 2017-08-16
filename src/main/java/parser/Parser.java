cb8b778f7bcd335098bd702c8cc03adc6a20c61c
Ivanisenko Roman <r.ivanisenko@gmail.com>
2017-08-16 05:09:39 +0300

    fix Parser

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -9,33 +9,40 @@ import storage.CoursesStorage;
 
 import java.io.IOException;
 
+
 /**
  * Class for parsing courses
  */
 public class Parser {
     private String url;
-    private Course course;
     private CoursesStorage coursesStorage;
 
     public Parser(String url) {
         this.url = url;
-        course = new Course();
         coursesStorage = new CoursesStorage();
     }
 
     private Document getPageWithCourses() throws IOException {
-        return Jsoup.connect(url).get();
+        return Jsoup.connect(url).
+                     validateTLSCertificates(false).
+                     get();
     }
 
     public void parsing() throws IOException {
         Document pageWithCourses = getPageWithCourses();
-        Elements liTags = pageWithCourses.getElementsByAttributeValue("li","courses-list--item _hparent ms2_product");
+        Elements liTags = pageWithCourses.
+                            getElementsByAttributeValue("class","courses-list--item" +
+                                                         " _hparent ms2_product");
 
         liTags.forEach(liTag -> {
             Element aTag = liTag.child(0);
             String urlOnCourse = aTag.attr("href");
+
+            Course course = new Course();
             course.setUrl(urlOnCourse);
+            coursesStorage.addCourse(course);
         });
+        coursesStorage.printAllCourses();
     }
 }
 
